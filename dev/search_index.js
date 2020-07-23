c408ba4cad7fe44e7d863320880876c00e9c1df9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-23 10:29:31 +0000

    build based on 8964012

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TutorialsForDevelopers","category":"page"},{"location":"#TutorialsForDevelopers","page":"Home","title":"TutorialsForDevelopers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TutorialsForDevelopers]","category":"page"}]
+}
